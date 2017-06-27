8ae65a994b99142170effe3408969a5bd4df4daa
Pranjal Jain <pranjaljn195@gmail.com>
2017-06-27 15:54:32 +0530

    Add files via upload

---
+++ tweetgeek.py
@@ -0,0 +1,31 @@
+from textblob import TextBlob
+import re
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean tweet text by removing links, special characters
+    using simple regex statements.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def get_tweet_sentiment(tweet):
+    '''
+    Utility function to classify sentiment of passed tweet
+    using textblob's sentiment method
+    '''
+    # create TextBlob object of passed tweet text
+    analysis = TextBlob(clean_tweet(tweet))
+    # set sentiment
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+
+if __name__ == "__main__":
+        print("Enter the tweet you want to classify")
+        tweet = input()
+        sentiment = get_tweet_sentiment(tweet)
+        print(sentiment)
+ 
